1ad0cd30132be5189ebed753b3509e80ac46bd5f
nourhanalturk <121407217+nourhanalturk@users.noreply.github.com>
2023-04-03 16:22:37 +0300

    Flutter

---
+++ lib/login_screen.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:nour/sharing/sharing.component/components.dart';
+
+class LoginScreen extends StatelessWidget {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  defaultFormField(
+                      controller: emailController,
+                      type: TextInputType.emailAddress,
+                      validate:(String? value){
+                        if(value!.isEmpty){
+                          return'email adress must not be empty';
+                        }
+                       return null;
+                      },
+                      text: 'Email adress ',
+                      prefex: Icons.email,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  defaultFormField(
+                      controller: passwordController,
+                      type: TextInputType.visiblePassword,
+                      validate: (String? value){
+                        if(value == null ||value!.isEmpty){
+                          return 'password??';
+                        }
+                        return null ;
+                      },
+                      text: 'password',
+                      prefex:Icons.lock,
+                    suffex: Icons.remove_red_eye,
+                    isPassword: true,
+
+                  ),
+                  // TextFormField(
+                  //   controller: passwordController,
+                  //   keyboardType: TextInputType.visiblePassword,
+                  //   obscureText: true,
+                  //   onFieldSubmitted: (String value) {
+                  //     print(value);
+                  //   },
+                  //   onChanged: (String value) {
+                  //     print(value);
+                  //   },
+                  //   validator: (value) {
+                  //     if(value!.isEmpty){
+                  //       return 'password must not be  empty';
+                  //     }
+                  //     return null;
+                  //   },
+                  //   decoration: InputDecoration(
+                  //     labelText: 'Password',
+                  //     prefixIcon: Icon(
+                  //       Icons.lock,
+                  //     ),
+                  //     suffixIcon: Icon(
+                  //       Icons.remove_red_eye,
+                  //     ),
+                  //     border: OutlineInputBorder(),
+                  //   ),
+                  // ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                 defaultButton(
+                     function:(){
+                       if(formkey.currentState!.validate!()){
+
+                       print(emailController.text);
+                       print(passwordController.text);
+                       }
+                     },
+                     text: 'login'
+                 ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account?',
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Register Now',
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/sharing/sharing.component/components.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget defaultButton({
+  double width = double.infinity,
+   Color color = Colors.blue,
+  required  Function function ,
+  required String text ,
+}) =>  Container(
+  width: width,
+  color: color,
+  child: MaterialButton(
+    onPressed:(){
+      function();
+  },
+    child: Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+);
+
+
+Widget defaultFormField ({
+ required TextEditingController controller ,
+  required TextInputType type ,
+  Function? onSubmit ,
+  Function? onChange,
+  bool isPassword =false,
+  required Function(String? s) validate,
+  required String text,
+  required IconData prefex,
+  IconData? suffex,
+
+
+}){
+  return TextFormField(
+    controller: controller,
+    obscureText: isPassword,
+    keyboardType: type,
+    onFieldSubmitted:(s){
+      onSubmit!(s);
+    },
+    onChanged:(s){
+      onChange!(s);
+    },
+    validator: (String? v){
+    validate(v);
+    },
+    decoration: InputDecoration(
+      labelText: text,
+      prefixIcon: Icon(
+        prefex,
+      ),
+      suffixIcon: suffex != null ?Icon(
+       suffex,
+      ) : null ,
+      border: OutlineInputBorder(),
+    ),
+  );
+}
